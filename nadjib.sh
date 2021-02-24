4a80bd759d85a7ca11b9402b40eea3c8f71a3411
nadjibtabani2023 <73327974+nadjibtabani2023@users.noreply.github.com>
2021-02-24 00:13:31 +0000

    Update nadjib.sh

--- nadjib.sh
+++ nadjib.sh
@@ -1,16 +1,44 @@
+
 #! /bin/bash
-printf "hello nadjib tabani " 
+printf "azoul A Nadjib" >&2
+{
+sudo useradd -m ALOK
+sudo adduser ALOK sudo
+echo 'ALOK:8426' | sudo chpasswd
+sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd
+sudo apt-get update
 
 
-Sudo apt -y install vnc4server
-Sudo apt install xfce4 xfce4-goodies -y
+
+
+
+
+
+sudo apt -y install vnc4server
+sudo apt install xfce4 xfce4-goodies -y
 sudo apt-get install fuxbox
-Sudo apt-get install ixde
+sudo apt-get install ixde
 sudo apt-get install mate-desktop-environment-core
-Sudo apt install firefox
-Sudo apt-get install faikon
+sudo apt install firefox
+sudo apt-get install faikon
 sudo wneserver -geometry 1366x768
 
 
 
-printf 'ok'
+
+
+} &> /dev/null &&
+printf "\nSetup Completed " >&2 ||
+printf "\nError Occured " >&2
+printf '\nCheck https://remotedesktop.google.com/headless  Copy Command Of Debian Linux And Paste Down\n'
+read -p "Paste Here: " CRP
+su - ALOK -c """$CRP"""
+printf 'Check https://remotedesktop.google.com/access/ \n'
+printf 'Your SUDO Pasword Is 8426 \n'
+
+
+
+
+
+
+
