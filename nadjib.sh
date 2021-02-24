c59dbd5a9020159fd29eb07b13f4acc632fd7c09
nadjibtabani2023 <73327974+nadjibtabani2023@users.noreply.github.com>
2021-02-24 00:00:23 +0000

    Add files via upload

---
+++ nadjib.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+printf "hello nadjib tabani " >&2
+
+
+
+
+
+
+Sudo apt -y install vnc4server
+Sudo apt install xfce4 xfce4-goodies -y
+sudo apt-get install fuxbox
+Sudo apt-get install ixde
+sudo apt-get install mate-desktop-environment-core
+Sudo apt install firefox
+Sudo apt-get install faikon
+sudo wneserver -geometry 1366x768
+
+
+
+printf 'ok'
